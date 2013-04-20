9087ed0ed6cf49e5005d22345ad1615e7aca6690
unknown <Administrator@.(none)>
2013-04-20 15:14:22 +0800

    add thread and time event

--- xmfy/main.py
+++ xmfy/main.py
@@ -8,8 +8,14 @@ Created on 2013-4-19
 
 from bespeak import test_despeak
 import time
+import datetime
+import thread
 
 if __name__ == '__main__':
-    for i in range(100):
-        test_despeak()
+    while True:
+        now =  datetime.datetime.now()
         time.sleep(1)
+        hour = str(now).split()[1]
+        if hour.startswith('19:58:00') or hour.startswith('19:59') or hour.startswith('20:00'):
+             for i in range(10):
+                 thread.start_new_thread(test_despeak,())
